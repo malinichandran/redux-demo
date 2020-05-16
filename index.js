7b2fe1bd0f2fb8737987d80fbc4685559aeb4a89
malinichandran <37134838+malinichandran@users.noreply.github.com>
2020-05-16 15:53:14 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,79 @@
+const redux= require('redux');
+const reduxLogger= require('redux-Logger')
+const BUY_CAKE = 'BUY_CAKE';
+const BUY_ICECREAM='BUY_ICECREAM';
+const createStore= redux.createStore
+const combineReducers= redux.combineReducers
+const applyMiddleware=redux.applyMiddleware
+const logger=reduxLogger.createLogger()
+
+
+function buyCake(){
+    return {
+        type:BUY_CAKE,
+        info:'First Redux Action'
+    }
+}
+function buyIcecream(){
+    return{
+        type:BUY_ICECREAM,
+        info:"second Redux Action"
+    }
+}
+
+//(previousState,action)=>newState
+
+// const initialCakeState={
+//     numOfCakes:10,
+//     numOfIcecreams:10
+// }
+const initialCakeState={
+    numOfCakes:10
+}
+const initialIceCreamState={
+    numOfIceCreams:10
+}
+// const reducer=(state=initialState,action)=>{
+//     switch(action.type){
+//         case BUY_CAKE:return{
+//             ...state,
+//             numOfCakes:state.numOfCakes-1
+//         }
+//         case BUY_ICECREAM:return{
+//             ...state,
+//             numOfIcecreams:state.numOfIcecreams-1
+//         }
+//         default:return state
+//     }
+// }
+const cakeReducer=(state=initialCakeState,action)=>{
+    switch(action.type){
+        case BUY_CAKE:return{
+            ...state,
+            numOfCakes:state.numOfCakes-1
+        }
+                default:return state
+    }
+}
+const iceCreamReducer=(state=initialIceCreamState,action)=>{
+    switch(action.type){
+            case BUY_ICECREAM:return{
+            ...state,
+            numOfIceCreams:state.numOfIceCreams-1
+        }
+        default:return state
+    }
+}
+const rootReducer=combineReducers({
+    cake:cakeReducer,
+    iceCream:iceCreamReducer
+})
+const store= createStore(rootReducer,applyMiddleware(logger));
+console.log("Initial State",store.getState())
+const unsubscribe=store.subscribe(()=>{})
+store.dispatch(buyCake())
+store.dispatch(buyCake())
+store.dispatch(buyCake())
+store.dispatch(buyCake())
+store.dispatch(buyIcecream())
+unsubscribe()
